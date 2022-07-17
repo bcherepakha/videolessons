73f42a1d5876ced125730b72cf882e1880e51c81
Boris Cherepakha <bcherepakha@gmail.com>
2022-07-17 13:07:36 +0300

    Meeting 6

--- Lesson12/timer.js
+++ Lesson12/timer.js
@@ -2,26 +2,87 @@ function Timer( rootEl ) {
     this.rootEl = rootEl;
     this.showEl = this.rootEl.querySelector('span');
     this.startBtn = this.rootEl.querySelector('[data-action="start"]');
+    this.pauseBtn = this.rootEl.querySelector('[data-action="pause"]');
+    this.resetBtn = this.rootEl.querySelector('[data-action="reset"]');
+    this.stopBtn = this.rootEl.querySelector('[data-action="stop"]');
 
     this.startTime = null;
+    this.shiftTime = 0;
     this.timerID = null;
+    this.status = Timer.STOP;
 
-    this.startBtn.addEventListener('click', this.run.bind(this));
+    if (this.startBtn) {
+        this.startBtn.addEventListener('click', this.run.bind(this));
+    }
+
+    if (this.pauseBtn) {
+        this.pauseBtn.addEventListener('click', this.pause.bind(this));
+    }
+
+    if (this.resetBtn) {
+        this.resetBtn.addEventListener('click', this.reset.bind(this));
+    }
+
+    if (this.stopBtn) {
+        this.stopBtn.addEventListener('click', this.stop.bind(this));
+    }
 
     this.render();
 }
 
+Timer.RUN = 'RUN';
+Timer.STOP = 'STOP';
+Timer.PAUSE = 'PAUSE';
+
 Timer.prototype.run = function () {
+    if (this.status === Timer.STOP) {
+        this.shiftTime = 0;
+    }
+
+    if (this.status === Timer.RUN) {
+        return ;
+    }
+
     this.startTime = Date.now();
     this.timerID = setInterval(this.render.bind(this), 1000);
+    this.status = Timer.RUN;
+
+    this.render();
 };
+
+Timer.prototype.pause = function () {
+    clearInterval(this.timerID);
+    this.timerID = null;
+
+    if (this.startTime) {
+        this.shiftTime = this.getTime();
+        this.startTime = null;
+    }
+
+    this.render();
+    this.status = Timer.PAUSE;
+}
+
+Timer.prototype.stop = function () {
+    this.pause();
+    this.status = Timer.STOP;
+}
+
+Timer.prototype.reset = function () {
+    this.startTime = null;
+    this.shiftTime = 0;
+    this.timerID = null;
+    this.status = Timer.STOP;
+
+    this.render();
+}
 
 Timer.prototype.getTime = function () {
     if (!this.startTime) {
-        return 0;
+        return this.shiftTime;
     }
 
-    return Date.now() - this.startTime;
+    return this.shiftTime + Date.now() - this.startTime;
 };
 
 
--- Meeting5/board.js
+++ Meeting5/board.js
@@ -1,24 +1,40 @@
-const EMPTY = '';
-const X = 'x';
-const O = 'o';
+function Board(selector) {
+    // this = {}
 
-const boardState = {
-    board: [
-        [EMPTY, EMPTY, EMPTY],
-        [EMPTY, EMPTY, EMPTY],
-        [EMPTY, EMPTY, EMPTY]
-    ],
-    currentUser: X,
+    this.gameEl = document.querySelector(selector);
+    this.boardEl = this.gameEl.querySelector('.cross__board');
+    this.cells = Array.from(this.boardEl.querySelectorAll('.cross__board-item'));
+
+    this.cells.forEach((cell, idx) => {
+        cell.addEventListener('click', this.step.bind(this,idx));
+    });
+
+    this.start();
 }
 
-boardState.boardEl = document.querySelector('.cross__board');
-boardState.cells = Array.from(boardState.boardEl.querySelectorAll('.cross__board-item'));
+Board.EMPTY = '';
+Board.X = 'x';
+Board.O = 'o';
+Board.ROW = 'ROW';
+Board.COLUMN = 'COLUMN';
+Board.DIAGONAL = 'DIAGONAL';
+Board.END_GAME = 'END_GAME';
+Board.STARTED_GAME = 'STARTED_GAME';
 
-boardState.cells.forEach((cell, idx) => {
-    cell.addEventListener('click', () => step(idx, boardState));
-});
+Board.prototype.start = function () {
+    this.board = [
+        [Board.EMPTY, Board.EMPTY, Board.EMPTY],
+        [Board.EMPTY, Board.EMPTY, Board.EMPTY],
+        [Board.EMPTY, Board.EMPTY, Board.EMPTY]
+    ];
+    this.currentUser = Board.X;
+    this.status = Board.STARTED_GAME;
+    this.winner = null;
 
-function renderX() {
+    this.clearBoard();
+}
+
+Board.prototype.renderX = function() {
     const svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 
     svgEl.setAttribute('class', 'x cross__board-item-el');
@@ -45,7 +61,7 @@ function renderX() {
     return svgEl;
 }
 
-function render0() {
+Board.prototype.render0 = function() {
     const svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 
     svgEl.setAttribute('class', 'o cross__board-item-el');
@@ -56,53 +72,144 @@ function render0() {
     return svgEl;
 }
 
-function clearBoard(boardState) {
-    boardState.cells.forEach(cell => {
+Board.prototype.clearBoard = function() {
+    this.cells.forEach(cell => {
         cell.innerText = '';
     });
 }
 
-function renderBoard(boardState) {
-    boardState.cells.forEach((cell, idx) => {
+Board.prototype.renderBoard = function() {
+    this.cells.forEach((cell, idx) => {
         if (cell.innerHTML !== '') {
             return ;
         }
 
         const row = Math.floor(idx / 3);
         const column = idx % 3;
-        const data = boardState.board[row][column];
+        const data = this.board[row][column];
 
-        if (data === X) {
-            cell.append( renderX() );
-        } else if (data === O) {
-            cell.append( render0() );
+        if (data === Board.X) {
+            cell.append( this.renderX() );
+        } else if (data === Board.O) {
+            cell.append( this.render0() );
         }
     });
 }
 
-function step(cellIdx, boardState) {
+Board.prototype.step = function (cellIdx) {
+    if (this.status === Board.END_GAME) {
+        return ;
+    }
+
     const row = Math.floor(cellIdx / 3);
     const column = cellIdx % 3;
-    const data = boardState.board[row][column];
+    const data = this.board[row][column];
 
-    if (data !== EMPTY) {
+    if (data !== Board.EMPTY) {
         throw new Error('Cell not empty. Step denided');
 
         return ;
     }
 
-    const { currentUser } = boardState;
+    const { currentUser } = this;
 
-    boardState.board[row][column] = currentUser;
-    boardState.currentUser = currentUser === X ? O : X;
+    // сохраняем ход
+    this.board[row][column] = currentUser;
 
-    renderBoard(boardState);
+    // перерисовка
+    this.renderBoard();
+
+    // проверяем победили или ничья
+    const lines = this.getLines();
+    const winLine = lines.find(line => this.isLineWin(line, currentUser));
+
+    if (winLine) {
+        // победа
+        this.status = Board.END_GAME;
+        this.winner = currentUser;
+        this.winAlert();
+
+        return ;
+    }
+
+    const lineWithEmptyCell = lines.find(this.hasEmptyCellInLine, this);
+
+    if (!lineWithEmptyCell) {
+        // ничья
+        this.status = Board.END_GAME;
+        this.winner = null;
+        this.drawAlert();
+
+        return ;
+    }
+
+    // если игра не закончена, то ход передается
+    this.currentUser = currentUser === Board.X ? Board.O : Board.X;
 }
 
-clearBoard(boardState);
-renderBoard(boardState);
+Board.prototype.drawAlert = function () {
+    alert(`Draw the game`);
+}
 
-console.log( boardState );
+Board.prototype.winAlert = function () {
+    alert(`${this.winner} win the game`);
+}
 
-// step(1, boardState);
-// step(4, boardState);
+Board.prototype.getLines = function () {
+    const lines = [];
+    const {board} = this;
+
+    for (let i=0; i < 3; i++) {
+        const row = {
+            type: Board.ROW,
+            idx: i,
+            data: [...board[i]]
+        };
+        const column = {
+            type: Board.COLUMN,
+            idx: i,
+            data: [
+                board[0][i],
+                board[1][i],
+                board[2][i],
+            ]
+        };
+
+        lines.push(row, column);
+    }
+
+    lines.push(
+        {
+            type: Board.DIAGONAL,
+            idx: 0,
+            data: [
+                board[0][0],
+                board[1][1],
+                board[2][2],
+            ]
+        },
+        {
+            type: Board.DIAGONAL,
+            idx: 1,
+            data: [
+                board[0][2],
+                board[1][1],
+                board[2][0],
+            ]
+        }
+    );
+
+    return lines;
+}
+
+Board.prototype.hasEmptyCellInLine = function (line) {
+    return line.data.some(symbol => symbol === Board.EMPTY);
+}
+
+Board.prototype.isLineWin = function(line, symbol) {
+    return line.data.every(currentSymbol => currentSymbol === symbol);
+}
+
+const currentBoard = new Board('.game');
+
+console.log( currentBoard );
